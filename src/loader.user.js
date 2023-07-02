f42be6c05e24530fc2871166431607346b8d9b56
Mai3122 <maisakurajima3119@gmail.com>
2023-07-01 23:43:57 -0300

    New Loader

--- src/loader.user.js
+++ src/loader.user.js
@@ -2,13 +2,13 @@
 // @name         MBS - Maid's Bondage Scripts
 // @namespace    MBS
 // @version      0.6.7
-// @description  Loader of Mai's Forked "Maid's Bondage Scripts" mod
+// @description  Loader of Mai's Custom MBS
 // @author       Bananarama92
 // @include      /^https:\/\/(www\.)?bondageprojects\.elementfx\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
 // @include      /^https:\/\/(www\.)?bondage-europe\.com\/R\d+\/(BondageClub|\d+)(\/((index|\d+)\.html)?)?$/
-// @homepage     https://github.com/Mai3122/MBS.git#readme
-// @source       https://github.com/Mai3122/MBS.git
-// @downloadURL  https://github.com/Mai3122/MBS/raw/main/src/loader.user.js
+// @homepage     https://github.com/Mai3122/MBSMai.git#readme
+// @source       https://github.com/Mai3122/MBSMai.git
+// @downloadURL  https://github.com/Mai3122/MBSMai/raw/main/src/loader.user.js
 // @run-at       document-end
 // @grant        none
 // ==/UserScript==
@@ -19,7 +19,7 @@ setTimeout(
         const n = document.createElement("script");
         n.language = "JavaScript";
         n.crossorigin = "anonymous";
-        n.src = "https://mai3122.github.io/MBS/main/mbs.js";
+        n.src = "https://mai3122.github.io/MBSMai/main/mbs.js";
         document.head.appendChild(n);
     },
     2000,
